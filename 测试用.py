8583b7a3f8ba982915b3cb8ac75f57fb7224e7d0
hanxfeng <3556050019@qq.com>
2026-01-10 20:31:22 +0800

    为Readme添加了接口的描述

--- 测试用.py
+++ 测试用.py
@@ -1,11 +1,5 @@
 import pymysql
-db_config = {
-    "host": "101.200.161.243",
-    "user": "root",
-    "password": "050316",
-    "database": "online_learning",
-    "charset": "utf8mb4"
-}
+
 conn = pymysql.connect(**db_config)
 cursor = conn.cursor()
 cursor.execute(
